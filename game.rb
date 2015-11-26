6e66fac7bac4d7113ccb92bb223e6a265731c9b7
CallumDougan <practicalitymalfunction@gmail.com>
2015-11-26 09:17:48 +0000

    fixed explanation of entries again

--- game.rb
+++ game.rb
@@ -31,7 +31,7 @@ class Game
   end
 
   def user_input
-    puts self.show_board
+    # puts self.show_board
     puts "Please enter your play in the format ROW,COLUMN e.g. 2,1, or ctrl-C to quit. 0,2 is top right: 2,0 is bottom left."
     played = gets.chomp.split(",")
     # if played = "quit"
@@ -39,13 +39,13 @@ class Game
     # end
     row = played[0].to_i
     column = played[1].to_i
+    if !sense_checker(row, column)    
+    return
+    end
     place_piece(row, column)
   end
 
   def place_piece (row, column)
-    if !sense_checker(row, column)    
-    return
-    end
     @board[ row ][ column ] = @pieces[ @turn % 2 ]
     puts "\nTurn ##{self.turn} played.\n\nTurn ##{self.turn+1}, #{@pieces[ @turn % 2  + 1]} to play.  Here is the board: \n\n"
     puts self.show_board
